aa10ad31d7568dc23b40b694130181e1dadd541c
Megha Hanuman <mehanuma@akamai.com>
2025-10-05 22:06:34 -0400

    Terraform Property Creation Exercise

---
+++ property.tf
@@ -0,0 +1,58 @@
+resource "akamai_cp_code" "my_cp_code" {
+  name        = "mehanuma-tfcpcode"
+  contract_id = "1-1NC95D"
+  group_id    = data.akamai_group.my_group_id.id
+  product_id  = "prd_Fresca"
+  timeouts {
+    update = "1h"
+  }
+}
+
+resource "akamai_edge_hostname" "my_ehn" {
+  edge_hostname = "mehanumatf.prlab.lol.edgesuite.net"
+  product_id  = "prd_Fresca"
+  contract_id = "1-1NC95D"
+  group_id    = data.akamai_group.my_group_id.id
+  ip_behavior = "IPV4"
+}
+
+data "akamai_property_rules_builder" "my_default_rule" {
+  rules_v2025_07_07 {
+    name      = "default"
+    is_secure = false
+    behavior {
+      origin {
+        origin_type                   = "CUSTOMER"
+        hostname                      = "juiceshopmehanuma.akaorigin.com"
+        forward_host_header           = "ORIGIN_HOSTNAME"
+        cache_key_hostname            = "REQUEST_HOST_HEADER"
+        compress                      = true
+        enable_true_client_ip         = false
+        http_port                     = 80
+      }
+    }
+    behavior {
+      cp_code {
+        value {
+          id   = akamai_cp_code.my_cp_code.id
+          name = "mehanuma-tfcpcode"
+        }
+      }
+    }
+  }
+}
+
+resource "akamai_property" "my_property" {
+  name          = "mehanuma2-terraform"
+  product_id    =  "prd_Fresca"
+  contract_id   = "1-1NC95D"
+  group_id      = data.akamai_group.my_group_id.id
+  rule_format   = "v2025-07-07"
+  rules= data.akamai_property_rules_builder.my_default_rule.json
+  version_notes = "Initial version"
+  hostnames {
+    cname_from             = "mehanumatf.prlab.lol"
+    cname_to               = akamai_edge_hostname.my_ehn.edge_hostname
+    cert_provisioning_type = "DEFAULT"
+  }
+}
